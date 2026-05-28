16a31421eb6dcda6ae00981de6c6ccc1c2365169
Basia <barbara.wachek@gmail.com>
2026-05-28 08:26:57 +0200

    poprawki

--- google_sheets.py
+++ google_sheets.py
@@ -2,29 +2,41 @@ import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 
-from config import CREDENTIALS_FILE
+from config import (
+    GOOGLE_CREDENTIALS,
+    GOOGLE_SHEET_ID
+)
 
-SHEET_ID = "1-lqkngW5dvekVcddgo9ocZp8OGMDufNVPF7vDL19sW8"
 
 SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets"
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
 ]
 
 
 def get_sheet():
-    creds = Credentials.from_service_account_file(
-        CREDENTIALS_FILE,
+
+    creds = Credentials.from_service_account_info(
+        GOOGLE_CREDENTIALS,
         scopes=SCOPES
     )
 
     client = gspread.authorize(creds)
-    sheet = client.open_by_key(SHEET_ID).sheet1
+
+    sheet = client.open_by_key(
+        GOOGLE_SHEET_ID
+    ).sheet1
 
     return sheet
 
 
+
 def ensure_headers(sheet):
-    if not sheet.row_values(1):
+
+    first_row = sheet.row_values(1)
+
+    if not first_row:
+
         sheet.append_row([
             "timestamp",
             "site_ok",
@@ -32,12 +44,14 @@ def ensure_headers(sheet):
             "info"
         ])
 
-
 def append_log(site_ok, search_ok, info):
-    # 👉 timestamp generowany tutaj, zawsze aktualny
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    timestamp = datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
 
     sheet = get_sheet()
+
     ensure_headers(sheet)
 
     sheet.append_row([
